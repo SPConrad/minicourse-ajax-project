c4978228e4275b7327715be05154d38baf58ec8a
Konrad9 <konrad9@gmail.com>
2016-07-20 15:01:44 -0400

    api first version implemented
    calls to google maps and NYT apis firing correctly

--- js/script.js
+++ js/script.js
@@ -11,9 +11,35 @@ function loadData() {
     $wikiElem.text("");
     $nytElem.text("");
 
+    var $streetAdd = $('#street').val();
+    var $cityAdd = $('#city').val();
+    var concatAdd = $streetAdd + ", " + $cityAdd;
+
+    var nytUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    nytUrl += '?' + $.param({'api-key': "ea708178954d4c4ab302f6d7e8c16d9b", 'q': $cityAdd});
+
+    console.log(nytUrl);
+
     // load streetview
 
+    concatAdd = "http://maps.googleapis.com/maps/api/streetview?size=600x300&location=" + concatAdd;
+
+    var fullAppend = '<img class="bgimg" src="' + concatAdd +'">';
+
     // YOUR CODE GOES HERE!
+
+    $body.append(fullAppend);
+
+    $.ajax({
+        dataType: "json",
+        url: nytUrl, 
+        method: 'GET',
+    }).done(function(result) {
+        console.log(result.response.docs[0]);
+    }).fail(function(err){
+        throw err;
+    })
+
 
     return false;
 };
